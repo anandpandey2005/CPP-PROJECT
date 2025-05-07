5ac20ea96d8e390b3c24db3786d261bd0691c3b9
Anand <anandpandey20005@gmail.com>
2025-05-08 01:04:26 +0530

    inorder post order recurssive way

---
+++ cppNotes/Tree/06Striver.cpp
@@ -0,0 +1,41 @@
+// ---------------   POSTORDER TRAVERSAL
+#include <iostream>
+using namespace std;
+class Tree
+{
+public:
+    int data;
+    Tree *left = NULL;
+    Tree *right = NULL;
+
+    Tree(int data) : data(data), left(nullptr), right(nullptr) {}
+    //--------------- RECURSSIVE WAY
+    void rPostorder(Tree *root)
+    {
+        if (!root)
+            return;
+
+                rPostorder(root->left);
+        rPostorder(root->right);
+        cout << root->data;
+    }
+    ~Tree();
+};
+
+int main()
+{
+    Tree *root = new Tree(1); // Root node
+
+    root->left = new Tree(2);  // Left child of root
+    root->right = new Tree(3); // Right child of root
+
+    root->left->left = new Tree(4);  // Left child of node 2
+    root->left->right = new Tree(5); // Right child of node 2
+
+    root->right->left = new Tree(6);  // Left child of node 3
+    root->right->right = new Tree(7); // Right child of node 3
+
+    root->rPostorder(root);
+
+    return 0;
+}
